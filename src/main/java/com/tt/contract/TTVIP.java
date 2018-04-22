9d3d5da0639bf50d03d165810b3fb7c7e4866926
finley <573108440@qq.com>
2018-04-23 02:31:54 +0800

    API

---
+++ src/main/java/com/tt/contract/TTVIP.java
@@ -0,0 +1,343 @@
+package com.tt.contract;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+import org.web3j.abi.EventEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.StaticArray2;
+import org.web3j.abi.datatypes.generated.StaticArray4;
+import org.web3j.abi.datatypes.generated.Uint16;
+import org.web3j.abi.datatypes.generated.Uint160;
+import org.web3j.abi.datatypes.generated.Uint64;
+import org.web3j.abi.datatypes.generated.Uint8;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tuples.generated.Tuple2;
+import org.web3j.tuples.generated.Tuple3;
+import org.web3j.tuples.generated.Tuple5;
+import org.web3j.tuples.generated.Tuple7;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 3.3.1.
+ */
+public class TTVIP extends Contract {
+    private static final String BINARY = null;
+
+    protected static final HashMap<String, String> _addresses;
+
+    static {
+        _addresses = new HashMap<>();
+    }
+
+    protected TTVIP(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected TTVIP(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public List<SignedEventResponse> getSignedEvents(TransactionReceipt transactionReceipt) {
+        final Event event = new Event("Signed", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint64>() {}));
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(event, transactionReceipt);
+        ArrayList<SignedEventResponse> responses = new ArrayList<SignedEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            SignedEventResponse typedResponse = new SignedEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse.firstParty = (String) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.secondParty = (String) eventValues.getIndexedValues().get(1).getValue();
+            typedResponse.timestamp = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Observable<SignedEventResponse> signedEventObservable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        final Event event = new Event("Signed", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint64>() {}));
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(event));
+        return web3j.ethLogObservable(filter).map(new Func1<Log, SignedEventResponse>() {
+            @Override
+            public SignedEventResponse call(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(event, log);
+                SignedEventResponse typedResponse = new SignedEventResponse();
+                typedResponse.log = log;
+                typedResponse.firstParty = (String) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.secondParty = (String) eventValues.getIndexedValues().get(1).getValue();
+                typedResponse.timestamp = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public List<TransformedEventResponse> getTransformedEvents(TransactionReceipt transactionReceipt) {
+        final Event event = new Event("Transformed", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint64>() {}));
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(event, transactionReceipt);
+        ArrayList<TransformedEventResponse> responses = new ArrayList<TransformedEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            TransformedEventResponse typedResponse = new TransformedEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse.from = (String) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.to = (String) eventValues.getIndexedValues().get(1).getValue();
+            typedResponse.timestamp = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Observable<TransformedEventResponse> transformedEventObservable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        final Event event = new Event("Transformed", 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint64>() {}));
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(event));
+        return web3j.ethLogObservable(filter).map(new Func1<Log, TransformedEventResponse>() {
+            @Override
+            public TransformedEventResponse call(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(event, log);
+                TransformedEventResponse typedResponse = new TransformedEventResponse();
+                typedResponse.log = log;
+                typedResponse.from = (String) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.to = (String) eventValues.getIndexedValues().get(1).getValue();
+                typedResponse.timestamp = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public RemoteCall<Tuple7<String, String, BigInteger, BigInteger, BigInteger, BigInteger, String>> contents(String param0, BigInteger param1) {
+        final Function function = new Function("contents", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(param0), 
+                new org.web3j.abi.datatypes.generated.Uint256(param1)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}, new TypeReference<Uint8>() {}, new TypeReference<Uint8>() {}, new TypeReference<Uint64>() {}, new TypeReference<Uint64>() {}, new TypeReference<Utf8String>() {}));
+        return new RemoteCall<Tuple7<String, String, BigInteger, BigInteger, BigInteger, BigInteger, String>>(
+                new Callable<Tuple7<String, String, BigInteger, BigInteger, BigInteger, BigInteger, String>>() {
+                    @Override
+                    public Tuple7<String, String, BigInteger, BigInteger, BigInteger, BigInteger, String> call() throws Exception {
+                        List<Type> results = executeCallMultipleValueReturn(function);
+                        return new Tuple7<String, String, BigInteger, BigInteger, BigInteger, BigInteger, String>(
+                                (String) results.get(0).getValue(), 
+                                (String) results.get(1).getValue(), 
+                                (BigInteger) results.get(2).getValue(), 
+                                (BigInteger) results.get(3).getValue(), 
+                                (BigInteger) results.get(4).getValue(), 
+                                (BigInteger) results.get(5).getValue(), 
+                                (String) results.get(6).getValue());
+                    }
+                });
+    }
+
+    public RemoteCall<List> getTransactions(String to) {
+        final Function function = new Function("getTransactions", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(to)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<StaticArray2<Uint64>>() {}));
+        return new RemoteCall<List>(
+                new Callable<List>() {
+                    @Override
+                    @SuppressWarnings("unchecked")
+                    public List call() throws Exception {
+                        List<Type> result = (List<Type>) executeCallSingleValueReturn(function, List.class);
+                        return convertToNative(result);
+                    }
+                });
+    }
+
+    public RemoteCall<Tuple2<BigInteger, BigInteger>> getAccountInfo() {
+        final Function function = new Function("getAccountInfo", 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint16>() {}, new TypeReference<Uint16>() {}));
+        return new RemoteCall<Tuple2<BigInteger, BigInteger>>(
+                new Callable<Tuple2<BigInteger, BigInteger>>() {
+                    @Override
+                    public Tuple2<BigInteger, BigInteger> call() throws Exception {
+                        List<Type> results = executeCallMultipleValueReturn(function);
+                        return new Tuple2<BigInteger, BigInteger>(
+                                (BigInteger) results.get(0).getValue(), 
+                                (BigInteger) results.get(1).getValue());
+                    }
+                });
+    }
+
+    public RemoteCall<TransactionReceipt> takerecord(String from, String to, BigInteger amount, BigInteger timestamp, String remark) {
+        final Function function = new Function(
+                "takerecord", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(from), 
+                new org.web3j.abi.datatypes.Address(to), 
+                new org.web3j.abi.datatypes.generated.Uint16(amount), 
+                new org.web3j.abi.datatypes.generated.Uint64(timestamp), 
+                new org.web3j.abi.datatypes.Utf8String(remark)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> signContract(String secondParty, BigInteger shareProfit, BigInteger expireYear, String remark) {
+        final Function function = new Function(
+                "signContract", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(secondParty), 
+                new org.web3j.abi.datatypes.generated.Uint8(shareProfit), 
+                new org.web3j.abi.datatypes.generated.Uint8(expireYear), 
+                new org.web3j.abi.datatypes.Utf8String(remark)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<Tuple3<String, BigInteger, BigInteger>> accounts(String param0) {
+        final Function function = new Function("accounts", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(param0)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Uint16>() {}, new TypeReference<Uint16>() {}));
+        return new RemoteCall<Tuple3<String, BigInteger, BigInteger>>(
+                new Callable<Tuple3<String, BigInteger, BigInteger>>() {
+                    @Override
+                    public Tuple3<String, BigInteger, BigInteger> call() throws Exception {
+                        List<Type> results = executeCallMultipleValueReturn(function);
+                        return new Tuple3<String, BigInteger, BigInteger>(
+                                (String) results.get(0).getValue(), 
+                                (BigInteger) results.get(1).getValue(), 
+                                (BigInteger) results.get(2).getValue());
+                    }
+                });
+    }
+
+    public RemoteCall<Tuple5<String, String, BigInteger, BigInteger, String>> transactions(BigInteger param0) {
+        final Function function = new Function("transactions", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(param0)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}, new TypeReference<Uint16>() {}, new TypeReference<Uint64>() {}, new TypeReference<Utf8String>() {}));
+        return new RemoteCall<Tuple5<String, String, BigInteger, BigInteger, String>>(
+                new Callable<Tuple5<String, String, BigInteger, BigInteger, String>>() {
+                    @Override
+                    public Tuple5<String, String, BigInteger, BigInteger, String> call() throws Exception {
+                        List<Type> results = executeCallMultipleValueReturn(function);
+                        return new Tuple5<String, String, BigInteger, BigInteger, String>(
+                                (String) results.get(0).getValue(), 
+                                (String) results.get(1).getValue(), 
+                                (BigInteger) results.get(2).getValue(), 
+                                (BigInteger) results.get(3).getValue(), 
+                                (String) results.get(4).getValue());
+                    }
+                });
+    }
+
+    public RemoteCall<TransactionReceipt> transform(String to, BigInteger amount, String remark) {
+        final Function function = new Function(
+                "transform", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(to), 
+                new org.web3j.abi.datatypes.generated.Uint16(amount), 
+                new org.web3j.abi.datatypes.Utf8String(remark)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<List> getContracts() {
+        final Function function = new Function("getContracts", 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<StaticArray4<Uint64>>() {}));
+        return new RemoteCall<List>(
+                new Callable<List>() {
+                    @Override
+                    @SuppressWarnings("unchecked")
+                    public List call() throws Exception {
+                        List<Type> result = (List<Type>) executeCallSingleValueReturn(function, List.class);
+                        return convertToNative(result);
+                    }
+                });
+    }
+
+    public RemoteCall<TransactionReceipt> addFund(String to, BigInteger amount, String remark) {
+        final Function function = new Function(
+                "addFund", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(to), 
+                new org.web3j.abi.datatypes.generated.Uint16(amount), 
+                new org.web3j.abi.datatypes.Utf8String(remark)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<BigInteger> test() {
+        final Function function = new Function("test", 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint160>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteCall<BigInteger> nounces(String param0) {
+        final Function function = new Function("nounces", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(param0)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint64>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public static RemoteCall<TTVIP> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(TTVIP.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<TTVIP> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(TTVIP.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static TTVIP load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new TTVIP(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public static TTVIP load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new TTVIP(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected String getStaticDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static String getPreviouslyDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static class SignedEventResponse {
+        public Log log;
+
+        public String firstParty;
+
+        public String secondParty;
+
+        public BigInteger timestamp;
+    }
+
+    public static class TransformedEventResponse {
+        public Log log;
+
+        public String from;
+
+        public String to;
+
+        public BigInteger timestamp;
+    }
+}
